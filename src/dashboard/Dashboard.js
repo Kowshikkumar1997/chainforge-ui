b13af2641700ceeebeb02bedd18b1655691908fd
Kowshikkumar <reddykowshikkm@gmail.com>
2026-01-09 18:00:30 +0000

    Update Dashboard.js

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -11,7 +11,7 @@ import axios from "axios";
  * This dashboard assumes contracts were generated and deployed via ChainForge.
  */
 
-import { API_BASE_URL } from "../config";
+const API_BASE_URL = "http://localhost:4000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
